262e25f7e19948b50d30ca438f3ce797a9e932c9
Tristan Sloughter <t@crashfast.com>
2019-12-26 08:01:12 -0700

    remove more mysql stuff in github actions

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,12 @@
 ExUnit.start()
 
-Application.ensure_all_started(:myxql)
+case System.get_env("GITHUB_ACTIONS", nil) do
+  nil ->
+    Application.ensure_all_started(:myxql)
+  _ ->
+    nil
+end
+
 Application.ensure_all_started(:postgrex)
 Application.ensure_all_started(:ecto_sql)
 Application.ensure_all_started(:erleans_provider_ecto)
